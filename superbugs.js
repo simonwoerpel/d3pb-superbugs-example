0296d848768a85c4956b590c46c274ab960b586f
Simon Wörpel <simon.woerpel@medienrevolte.de>
2017-01-26 15:49:56 +0100

    Update because of new versions of `d3-playbooks`

--- superbugs.js
+++ superbugs.js
@@ -1,31 +1,44 @@
-d3.playbooks.choroplethMap({
-  width: 800,
-  height: 800,
-  elementId: 'superbugs-map',
-  cssNamespace: 'superbugs-map',
-  dataUrl: './data/e-coli.csv',
-  geoDataUrl: './data/europe.topo.json',
-  responsiveSvg: true,
-  isTopojson: true,
-  topojsonLayerName: 'europe_clipped',
-  getId: f => f.properties.iso_a2,
-  yExtent: [0, 64],
-}).render().infobox({
-  element: '#superbugs-map__infobox',
-  template: `
-    <h3>{name}</h3>
-    <p class="infobox__data">{display_value}</p>
-    <h4>Escherichia coli vs cephalosporins</h4>
-    <p class="infobox__subtitle">Resistance to 3rd generation cephalosporins in percent.
-    Of all infections with this bacterium, this percentage was resistant to this antibiotic.</p>
-    <p class="infobox__eudata">EU: 12 %</p>
-    <p class="infobox__annotation">ECDC Surveillance report 2014, except Poland (2013)</p>
-  `
-}).selector({
-  element: '#superbugs-map__selector',
-  getLabel: f => f.name
-}).legend({
-  element: '#superbugs-map__legend',
-  wrapperTemplate: '<ul name="legend">{body}</ul>',
-  itemTemplate: '<li style="background-color:{color}">{label} %</li>'
-})
+d3.playbooks.superbugsMap = ({
+  title,
+  description,
+  annotation,
+  eudata,
+  dataUrl,
+  yExtent
+}) => {
+
+  const superbugsMap = d3.playbooks.choroplethMap({
+    width: 800,
+    height: 800,
+    elementId: 'superbugs-map',
+    cssNamespace: 'superbugs-map',
+    geoDataUrl: './data/europe.topo.json',
+    dataUrl,
+    yExtent,
+    nullColor: '#eee',
+    responsiveSvg: true,
+    isTopojson: true,
+    topojsonLayerName: 'europe_clipped',
+    getId: f => f.properties.iso_a2,
+  })
+
+  superbugsMap.render().infobox({
+    element: '#superbugs-map__infobox',
+    template: `
+      <h3>{name}</h3>
+      <p class="infobox__data">{display_value}</p>
+      <h4>${title}</h4>
+      <p class="infobox__subtitle">${description}</p>
+      <p class="infobox__eudata">EU: ${eudata}</p>
+      <p class="infobox__annotation">${annotation}</p>
+    `
+  }).selector({
+    element: '#superbugs-map__selector',
+    getLabel: f => f.name
+  }).legend({
+    element: '#superbugs-map__legend',
+    wrapperTemplate: '<ul name="legend">{body}</ul>',
+    itemTemplate: '<li style="background-color:{color}">{label} %</li>'
+  })
+
+}
